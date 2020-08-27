5aff8f83e778e248d5269ccb9da2d0374b9418bc
huangal5 <alisa.y.s.huang@oocl.com>
2020-08-27 17:56:15 +0800

    [exercise][Alisa] test: html render: Customer BigCo has three performances and the audience is add more than 30

--- test/statementTest.js
+++ test/statementTest.js
@@ -184,3 +184,37 @@ test('Customer BigCo without performance. ', t => {
   t.is(result, 'Statement for BigCo\nAmount owed is $0.00\nYou earned 0 credits \n');
 });
 
+test('html render: Customer BigCo has three performances and the audience is add more than 30 ', t => {
+  //given
+  const invoice = {
+    'customer': 'BigCo',
+    'performances': [
+      {
+        'playID': 'hamlet',
+        'audience': 55,
+      },
+      {
+        'playID': 'as-like',
+        'audience': 35,
+      },
+      {
+        'playID': 'othello',
+        'audience': 40,
+      },
+    ],
+  };
+
+  //when
+  const result = statementHtml(invoice, plays);
+
+  t.is(result, '<h1>Statement for BigCo</h1>\n' +
+    '<table>\n' +
+    '<tr><th>play</th><th>seats</th><th>cost</th></tr>' +
+    ' <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n' +
+    ' <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n' +
+    ' <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n' +
+    '</table>\n' +
+    '<p>Amount owed is <em>$1,730.00</em></p>\n' +
+    '<p>You earned <em>47</em> credits</p>\n');
+
+});
